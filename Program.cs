97c7d5ee1bdbccd8fac0f339ab68cdb4bb8b3a66
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-06 10:02:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("this is a while loop");
+
+            while (i <=5) 
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("this is a do while loop");
+
+            int j = 0;
+
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j <=5);
+
+            Console.ReadLine();
+        }
+    }
+}
